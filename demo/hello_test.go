1f9c0de56edfc3ae8f4efaebfd5dfeefe33807de
流光 <67579722@qq.com>
2021-03-21 22:29:38 +0800

    fix

--- demo/hello_test.go
+++ demo/hello_test.go
@@ -7,6 +7,6 @@ import (
 
 func TestHello(t *testing.T) {
 	assertTool := assert.New(t)
-	assertTool.Equal(123,123, "they should be equal")
-	assertTool.Equal(4, hello(2))
+	assertTool.Equal(123, 123, "they should be equal")
+	assertTool.Equal(3, hello(2))
 }
