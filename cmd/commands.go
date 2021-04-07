aef7e29bc79d9c4454421dd4e8666af6c12c3fa7
流光 <67579722@qq.com>
2021-04-07 18:24:36 +0800

    可使用环境变量自定义.env文件名称

--- cmd/commands.go
+++ cmd/commands.go
@@ -4,6 +4,7 @@ import (
 	"github.com/joho/godotenv"
 	"github.com/liuguangw/forumx/cmd/migrate"
 	"github.com/urfave/cli/v2"
+	"os"
 )
 
 func prepareMainApp() *cli.App {
@@ -18,9 +19,19 @@ func prepareMainApp() *cli.App {
 	return mainApp
 }
 
+//加载 `.env`文件, 可以使用 `FORUM_ENV_FILENAME` 环境变量设置自定义的文件名
+func loadEnvFile() error {
+	//自定义.env文件名
+	envFileName := os.Getenv("FORUM_ENV_FILENAME")
+	if envFileName != "" {
+		return godotenv.Load(envFileName)
+	}
+	return godotenv.Load()
+}
+
 func Execute(args []string) error {
-	//加载.env环境变量配置
-	if err := godotenv.Load(); err != nil {
+	//加载环境变量文件
+	if err := loadEnvFile(); err != nil {
 		return err
 	}
 	mainApp := prepareMainApp()
